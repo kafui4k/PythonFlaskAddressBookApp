fb124c6b79fc5fc96d49229a8c9941f9101d7586
Kafui Alordo <kalordo7@gmail.com>
2018-10-22 22:10:22 +0000

    index route to display list of all contacts

--- application.py
+++ application.py
@@ -1,24 +1,15 @@
 from flask import Flask, render_template, request
+from contactform import AddContact
 
 #app instance
 app = Flask(__name__)
+app.secret_key = 'development key'
 
-#hompage method
+#hompage route
 @app.route('/')
-def home():
+def index():
     return render_template('index.html')
 
-#add contact page method 
-@app.route('/addcontact', methods=['GET', 'POST'])
-def addcontact():
-    if request.method == 'POST': #this execute upon form submit - by button send.
-        return "Success"
-    return render_template('addcontact.html')    
-
-#add search page method 
-@app.route('/searchcontact')
-def searchcontact():
-    return render_template('searchcontact.html')
 
 #main
 if __name__ == '__main__':
