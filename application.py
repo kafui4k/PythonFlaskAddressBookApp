48b77648252843458b0d1041c2404ebc3b23e9c3
Kafui Alordo <kalordo7@gmail.com>
2018-10-23 13:56:51 +0000

    edit route

--- application.py
+++ application.py
@@ -1,13 +1,19 @@
 from flask import Flask, render_template, request
-from contactform import AddContact
+
+
+
+
 
 #app instance
 app = Flask(__name__)
+
 app.secret_key = 'development key'
 
-#hompage route
+#home
 @app.route('/')
 def index():
+    #retrieve list of all contacts
+    
     return render_template('index.html')
 
 
